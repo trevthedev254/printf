b05ac63e45056b47b9b9c5fa0e1bc1b643168192
trevthedev254 <trevetech@gmail.com>
2022-05-20 20:04:35 +0000

    struct on main.h and printf

--- _printf.c
+++ _printf.c
@@ -8,5 +8,15 @@
  */
 int _printf(const char *format, ...)
 {
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
 
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
+
+
+	va_end(valist);
+	return (count);
 }
--- main.h
+++ main.h
@@ -1,6 +1,16 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 #include <stdarg.h>
+/**
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
+ */
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} print_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
