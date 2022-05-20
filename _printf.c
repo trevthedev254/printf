f26dc3d02df78ee9c88a8d349372212dc46c4f8f
trevthedev254 <trevetech@gmail.com>
2022-05-20 19:50:38 +0000

    working on printf.c

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - prints characters
+ * @format: array of argument types to print
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+
+}
