7d0a521941e1cc581777957d2b38cc201cb9fb16
trevthedev254 <trevetech@gmail.com>
2022-05-20 20:11:21 +0000

    task 0 trial

---
+++ 0_characters.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_c - prints a char
+ * @c: char to print
+ *
+ * Return: always 1
+ */
+int print_c(va_list c)
+{
+	char ch = (char)va_arg(c, int);
+
+	_putchar(ch);
+	return (1);
+}
+
+/**
+ * print_s - prints a string
+ * @s: printed string
+ *
+ * Return: number of chars printed
+ */
+int print_s(va_list s)
+{
+	int count;
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (count = 0; str[count]; count++)
+	{
+		_putchar(str[count]);
+	}
+	return (count);
+}
